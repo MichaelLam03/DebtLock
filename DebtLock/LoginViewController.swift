9b925542562c9d80e32bd8d5d989a6711ceb9673
MichaelLam03 <30397380+MichaelLam03@users.noreply.github.com>
2017-07-25 16:46:22 +0800

    Commit errored firebase login

--- DebtLock/LoginViewController.swift
+++ DebtLock/LoginViewController.swift
@@ -8,7 +8,32 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+
+
+func loginButtonTapped(_ sender: UIButton) {
+    // 1
+    guard let authUI = FUIAuth.defaultAuthUI()
+        else { return }
+    
+    // 2
+    authUI.delegate = self
+    
+    // 3
+    let authViewController = authUI.authViewController()
+    present(authViewController, animated: true)
+}
 
 class LoginViewController: UIViewController {
-    
+    // ...
 }
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        print("handle user signup / login")
+    }
+}
+
+
+
