f35652c7e3d450c7f54525948a6c4c0a99e0ea0f
Ji Yeon Kim <19kimj4@wis.edu.hk>
2017-07-26 13:53:47 +0800

    commit

--- DebtLock/Debt Service.swift
+++ DebtLock/Debt Service.swift
@@ -7,12 +7,20 @@
 //
 
 import Foundation
-import FirebaseDatabase
 import FirebaseAuth.FIRUser
+import FirebaseDatabase
 
 struct DebtService {
     
-    func create() {
-        let ref = Database.database().reference().child("DEBT").childByAutoId()
-
-    }}
+        static func create(_ firUser: FIRUser, username: String, completion: @escaping (User?) -> Void) {
+        let userAttrs = ["username": username]
+        
+        let ref = Database.database().reference().child("debts")
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            
+            
+            }}}
