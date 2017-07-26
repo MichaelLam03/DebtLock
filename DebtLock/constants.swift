f592bf3b8b3b634d42fafe02c298d9ee5a3aece4
Ji Yeon Kim <19kimj4@wis.edu.hk>
2017-07-26 09:58:28 +0800

    commit login retry

---
+++ DebtLock/constants.swift
@@ -0,0 +1,21 @@
+//
+//  constants.swift
+//  DebtLock
+//
+//  Created by Ji Yeon Kim on 26/7/2017.
+//  Copyright © 2017 Michael Lam. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Segue {
+        static let toCreateUsername = "toCreateUsername"
+    }
+    
+    struct UserDefaults {
+        static let currentUser = "currentUser"
+        static let uid = "uid"
+        static let username = "username"
+    }
+}
